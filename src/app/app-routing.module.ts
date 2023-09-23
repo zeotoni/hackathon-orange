211e7445d5433adf922380b53d7eebef9b58c59a
zeotoni <zeotoni666@gmail.com>
2023-09-23 10:11:02 -0300

    add login component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AddEventComponent } from './pages/add-event/add-event.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,11 @@ const routes: Routes = [
     path: 'add-event',
     component: AddEventComponent,
     title: 'Adicionar Evento'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
   }
 ];
 
