452e9e35bba1fd8f51c7004ff040499f172cbf80
zeotoni <zeotoni666@gmail.com>
2023-09-23 10:08:38 -0300

    add login file test

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [
+        LoginComponent,
+        ButtonComponent
+      ]
     })
     .compileComponents();
 
