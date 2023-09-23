8223492bb8d8a61065c1e133d4d3a05c15f51041
zeotoni <zeotoni666@gmail.com>
2023-09-23 06:46:27 -0300

    add folders of shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ContainerComponent } from './shared/components/container/container.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { FormComponent } from './shared/components/form/form.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    CardComponent,
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
