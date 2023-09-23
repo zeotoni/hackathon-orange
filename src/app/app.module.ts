52e7ecb4570925a1d49c09eec27c3a1477b189c4
zeotoni <zeotoni666@gmail.com>
2023-09-23 13:48:08 -0300

    remove button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { ContainerComponent } from './shared/components/container/container.component';
 import { CardComponent } from './shared/components/card/card.component';
 import { FormComponent } from './shared/components/form/form.component';
-import { ButtonComponent } from './shared/components/button/button.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AddEventComponent } from './pages/add-event/add-event.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -22,7 +21,6 @@ import { DetailsComponent } from './pages/details/details.component';
     ContainerComponent,
     CardComponent,
     FormComponent,
-    ButtonComponent,
     HomeComponent,
     AddEventComponent,
     LoginComponent,
--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,8 +9,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        LoginComponent,
-        ButtonComponent
+        LoginComponent
       ]
     })
     .compileComponents();
