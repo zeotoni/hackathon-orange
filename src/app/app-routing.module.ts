2db88c8d5014a5d98f693c6d6e5e005cc5fb74ab
zeotoni <zeotoni666@gmail.com>
2023-09-23 09:50:06 -0300

    add add-event component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AddEventComponent } from './pages/add-event/add-event.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'add-event',
+    component: AddEventComponent,
+    title: 'Adicionar Evento'
   }
 ];
 
