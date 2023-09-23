79ab0ef2bb10e791552f00db5e5542fceb1f5360
zeotoni <zeotoni666@gmail.com>
2023-09-23 06:52:25 -0300

    add new title and change title test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hackaton'`, () => {
+  it(`should have as title 'Agenda Tech'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hackaton');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hackaton app is running!');
+    expect(app.title).toEqual('Agenda Tech');
   });
 });
